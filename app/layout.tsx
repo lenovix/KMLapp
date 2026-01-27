5f3b84e53c7218199f5975deb500b056dacf9f6e
Ichsanul Kamil Sudarmi <ichkmlsdr@gmail.com>
2026-01-27 20:04:12 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen">
+      <body className="">
         <div className="flex flex-col bg-linear-to-br from-zinc-950 via-zinc-900 to-zinc-950 text-zinc-100">
           {children}
         </div>
